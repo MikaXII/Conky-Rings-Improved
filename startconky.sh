afd268b77dd17b9d0bc767ce48c62cefb5f07603
Mikael JOUBERT <mikael.joubert@planisware.com>
2020-04-24 11:09:23 +0200

    Update some missing elements

--- startconky.sh
+++ startconky.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 sleep 10 #time (in s) for the DE to start; use ~20 for Gnome or KDE, less for Xfce/LXDE etc
-conky -c ~/.conky/rings & # the main conky with rings
+conky -c ~/.conky/Conky-Rings-Improved/rings & # the main conky with rings
 sleep 8 #time for the main conky to start; needed so that the smaller ones draw above not below (probably can be lower, but we still have to wait 5s for the rings to avoid segfaults)
-conky -c ~/.conky/cpu &
+conky -c ~/.conky/Conky-Rings-Improved/cpu &
 sleep 1
-conky -c ~/.conky/mem &
-conky -c ~/.conky/notes &
+conky -c ~/.conky/Conky-Rings-Improved/mem &
+conky -c ~/.conky/Conky-Rings-Improved/notes &
+conky -c ~/.conky/Conky-Rings-Improved/rss &
+#conky -c ~/.conky/Conky-Rings-Improved/spotify &
